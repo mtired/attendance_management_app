79ca1317b70d2e5774162c801b8513aa43502a39
mtired <shindoi.misato@gmail.com>
2026-03-08 18:28:27 +0900

    登録処理テスト追加

---
+++ src/tests/Feature/UserLoginValidationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class UserLoginValidationTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
